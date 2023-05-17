a9290100d3ddcaf7719a12176301e6aae573603b
Maksim Pischulenok <pischule@outlook.com>
2023-05-17 21:42:24 +0300

    Add auth_date check

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,15 +4,21 @@ import type { Handle } from '@sveltejs/kit';
 bot.init();
 
 export const handle = (async ({ event, resolve }) => {
+	const cookieMaxAge = 60 * 60 * 12;
 	if (event.url.searchParams.has('hash')) {
-		event.cookies.set('tg-auth', event.url.search, { path: '/', maxAge: 60 * 60 * 24 });
+		event.cookies.set('tg-auth', event.url.search, { path: '/', maxAge: cookieMaxAge });
 	}
 
 	const auth = event.cookies.get('tg-auth');
 	if (auth) {
 		const authData = new Map(new URLSearchParams(auth).entries());
 		if (await bot.isSignatureValid(authData)) {
-			event.locals.userId = <string>authData.get('id');
+			const authDate = parseInt(<string>authData.get('auth_date'));
+			const nowDate = Date.now() / 1000;
+			const isNotStale = nowDate - authDate < cookieMaxAge * 2;
+			if (isNotStale) {
+				event.locals.userId = <string>authData.get('id');
+			}
 		}
 	}
 
