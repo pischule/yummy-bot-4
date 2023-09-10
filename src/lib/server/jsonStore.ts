7e04332256679123342484ad7d26d8aeed7a95e5
Maksim Pischulenok <pischule@outlook.com>
2023-09-10 14:39:08 +0300

    Migrate to bun's file api

--- src/lib/server/jsonStore.ts
+++ src/lib/server/jsonStore.ts
@@ -1,9 +1,8 @@
-import fs from 'fs/promises';
-
 export const jsonStore = <Type>(filename: string) => {
   const get = async () => {
     try {
-      return <Type>JSON.parse(await fs.readFile(filename, 'utf-8'));
+      const file = Bun.file(filename);
+      return <Type>await file.json();
     } catch (e) {
       console.error(`failed to read ${filename}`, e);
       return null;
@@ -12,7 +11,7 @@ export const jsonStore = <Type>(filename: string) => {
 
   const set = async (value: Type) => {
     try {
-      await fs.writeFile(filename, JSON.stringify(value));
+      await Bun.write(filename, JSON.stringify(value));
     } catch (e) {
       console.error(`failed to write ${filename}`, e);
     }
